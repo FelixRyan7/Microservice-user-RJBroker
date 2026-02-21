23bce2ec69afa738f43f82ee102148e8c0bf80bc
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:53:34 +0100

    service por user model

--- src/main/java/com/broker/user_service/Service/UserService.java
+++ src/main/java/com/broker/user_service/Service/UserService.java
@@ -1,11 +1,13 @@
 package com.broker.user_service.Service;
 
 import com.broker.user_service.DTOs.RegisterRequest;
+import com.broker.user_service.DTOs.UserBasicInfo;
 import com.broker.user_service.DTOs.UserDTO;
 import com.broker.user_service.model.User;
 import com.broker.user_service.model.UserPermission;
 import com.broker.user_service.repository.UserPermissionRepository;
 import com.broker.user_service.repository.UserRepository;
+import com.broker.user_service.utils.CurrentUserProvider;
 import com.broker.user_service.utils.ProductPermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,9 +21,10 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private UserPermissionRepository userPermissionRepository;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private CurrentUserProvider currentUserProvider;
 
     public UserDTO register(String username, String email, String password){
         if (userRepository.findByEmail(email).isPresent()) {
@@ -35,7 +38,6 @@ public class UserService {
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
-
         User savedUser = userRepository.save(user);
         assignDefaultPermissions(user);
 
@@ -53,4 +55,18 @@ public class UserService {
             userPermissionRepository.save(up);
         }
     }
+
+    public UserBasicInfo getFreshData(){
+        Long userId = currentUserProvider.getUserId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        return new UserBasicInfo(
+                user.getId(),
+                user.getUsername(),
+                user.isHasPersonalData()
+        );
+
+    }
+
 }
