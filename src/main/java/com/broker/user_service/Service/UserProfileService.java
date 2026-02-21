f1713401d7e8eaf5c7ba6f90ecb9d3448f32570e
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:51:44 +0100

    service for userprofile

--- src/main/java/com/broker/user_service/Service/UserProfileService.java
+++ src/main/java/com/broker/user_service/Service/UserProfileService.java
@@ -2,6 +2,7 @@ package com.broker.user_service.Service;
 
 import com.broker.user_service.DTOs.UserProfileRequest;
 import com.broker.user_service.DTOs.UserProfileResponse;
+import com.broker.user_service.events.UserCreatedEvent;
 import com.broker.user_service.model.User;
 import com.broker.user_service.model.UserPermission;
 import com.broker.user_service.model.UserProfile;
@@ -11,7 +12,11 @@ import com.broker.user_service.repository.UserRepository;
 import com.broker.user_service.utils.CurrentUserProvider;
 import com.broker.user_service.utils.ProductPermission;
 import jakarta.transaction.Transactional;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserProfileService {
@@ -19,23 +24,20 @@ public class UserProfileService {
     private final UserProfileRepository repository;
     private final UserRepository userRepository;
     private final CurrentUserProvider currentUserProvider;
+    private final KafkaTemplate<String, UserCreatedEvent> kafkaTemplate;
 
-
-
-    public UserProfileService(UserProfileRepository repository, UserRepository userRepository, CurrentUserProvider currentUserProvider) {
+    public UserProfileService(UserProfileRepository repository, UserRepository userRepository, CurrentUserProvider currentUserProvider, KafkaTemplate<String, UserCreatedEvent> kafkaTemplate) {
         this.repository = repository;
         this.userRepository = userRepository;
         this.currentUserProvider = currentUserProvider;
-
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    @Transactional
     public UserProfileResponse createProfile(UserProfileRequest request) {
 
         Long userId = currentUserProvider.getUserId();
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
 
         UserProfile profile = new UserProfile();
         profile.setUser(user);
@@ -59,10 +61,31 @@ public class UserProfileService {
 
         user.setHasPersonalData(true);
         userRepository.save(user);
+        UserCreatedEvent event = new UserCreatedEvent(
+                user.getId(),
+                user.getEmail(),
+                profile.getBaseCurrency()
+        );
+        kafkaTemplate.send("user-created-topic", String.valueOf(user.getId()), event)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        System.out.println("❌ ERROR enviando evento a Kafka");
+                        ex.printStackTrace();
+                    } else {
+                        System.out.println(
+                                "✅ Evento enviado a Kafka | topic="
+                                        + result.getRecordMetadata().topic()
+                                        + " | partition=" + result.getRecordMetadata().partition()
+                                        + " | offset=" + result.getRecordMetadata().offset()
+                        );
+                    }
+                });
 
+        return new UserProfileResponse(profile.getId(),profile.getFullName());
+    }
 
-
-        return new UserProfileResponse(profile.getId(),profile.getFullName(), profile.getUserLevel());
-
+    public String getFullNameSafe() {
+        Long userId = currentUserProvider.getUserId();
+        return repository.findFullNameByUserId(userId).orElse("");
     }
 }
