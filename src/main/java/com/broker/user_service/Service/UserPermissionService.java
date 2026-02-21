d76298d15d581fbfccce0cfc9c600b288365d670
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:54:03 +0100

    service for user permission

--- src/main/java/com/broker/user_service/Service/UserPermissionService.java
+++ src/main/java/com/broker/user_service/Service/UserPermissionService.java
@@ -1,2 +1,30 @@
-package com.broker.user_service.Service;public class UserPermissionService {
+package com.broker.user_service.Service;
+
+import com.broker.user_service.DTOs.UserProfileRequest;
+import com.broker.user_service.DTOs.UserProfileResponse;
+import com.broker.user_service.model.UserPermission;
+import com.broker.user_service.repository.UserPermissionRepository;
+import com.broker.user_service.utils.CurrentUserProvider;
+import com.broker.user_service.utils.ProductPermission;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserPermissionService {
+
+    @Autowired
+    private UserPermissionRepository userPermissionRepository;
+    @Autowired
+    private CurrentUserProvider currentUserProvider;
+
+    public List<ProductPermission> getPermissions() {
+        Long userId = currentUserProvider.getUserId();
+        List<UserPermission> userPermissions = userPermissionRepository.findByUserId(userId);
+        List<ProductPermission> permissions = userPermissions.stream()
+                .map(UserPermission::getPermission)
+                .toList();
+        return (permissions);
+    }
 }
