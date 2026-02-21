f4e8f0d1b999dd3f1b011334ebc2e02d6022751f
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:52:19 +0100

    config for kafka producer

--- src/main/java/com/broker/user_service/config/KafkaProducerConfig.java
+++ src/main/java/com/broker/user_service/config/KafkaProducerConfig.java
@@ -1,2 +1,38 @@
-package com.broker.user_service.config;public class KafkaProducerConfig {
+package com.broker.user_service.config;
+
+
+import com.broker.user_service.events.UserCreatedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    private final String bootstrapServers = "kafka:9092";
+
+    @Bean
+    public ProducerFactory<String, UserCreatedEvent> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        // para que el JsonSerializer no agregue headers que compliquen la deserialización
+        configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
+
