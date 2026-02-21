22025b5ed08edcf9604e8d5675b243f48b9469f4
FelixRyan7 <fpasadase@uoc.edu>
2026-02-21 16:49:30 +0100

    repository for model userProfileRepository

--- src/main/java/com/broker/user_service/repository/UserProfileRepository.java
+++ src/main/java/com/broker/user_service/repository/UserProfileRepository.java
@@ -2,6 +2,8 @@ package com.broker.user_service.repository;
 
 import com.broker.user_service.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,9 @@ import java.util.Optional;
 public interface UserProfileRepository
         extends JpaRepository<UserProfile, Long> {
     Optional<UserProfile> findByUserId(Long userId);
+
+    @Query("SELECT u.fullName FROM UserProfile u WHERE u.user.id = :userId")
+    Optional<String> findFullNameByUserId(@Param("userId") Long userId);
+
+
 }
